e97b0bc32e923aeed989fdb0e87844e0a8ab487d
Akash Mishra <167092640+Akashmishra06@users.noreply.github.com>
2025-03-26 10:53:47 +0000

    backtesToolsInstall

---
+++ backtestToolsInstall/main.py
@@ -0,0 +1,13 @@
+# backtestTools updated to 4.0.1
+
+# Install Command (virtual environment)
+pip install git+https://github.com/MudrakshnMcShaw/backtestTools.git
+
+# Update Command (virtual environment)
+pip install --upgrade git+https://github.com/MudrakshnMcShaw/backtestTools.git
+
+# Documentation
+# https://github.com/MudrakshnMcShaw/backtestTools/blob/master/README.md
+
+# Changelog:
+# - Fixed bugs
